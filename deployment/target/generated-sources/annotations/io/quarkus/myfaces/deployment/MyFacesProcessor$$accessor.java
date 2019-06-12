814940da93dbd2ff9792fb484d7f4be2415260f6
rafael-pestano <rafael-pestano@procergs.rs.gov.br>
2019-06-12 10:27:12 -0300

    remove arget folder add gitignore

--- deployment/target/generated-sources/annotations/io/quarkus/myfaces/deployment/MyFacesProcessor$$accessor.java
+++
@@ -1,7 +0,0 @@
-package io.quarkus.myfaces.deployment;
-public final class MyFacesProcessor$$accessor {
-    private MyFacesProcessor$$accessor() {}
-    public static Object construct() {
-        return new MyFacesProcessor();
-    }
-}
--- runtime/target/generated-sources/annotations/io/quarkus/myfaces/runtime/MyFacesTemplate$$accessor.java
+++
@@ -1,7 +0,0 @@
-package io.quarkus.myfaces.runtime;
-public final class MyFacesTemplate$$accessor {
-    private MyFacesTemplate$$accessor() {}
-    public static Object construct() {
-        return new MyFacesTemplate();
-    }
-}
